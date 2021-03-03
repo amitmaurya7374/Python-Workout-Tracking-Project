0279a2e969b67ae025fdff92e893bea46344c5f6
amitmaurya7374 <amitmaurya7374@gmail.com>
2021-03-03 12:31:41 +0530

    added data to google sheets with auth token

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 we are using nutritionix api for workout tracking.
 
 """
+from datetime import datetime
+
 import requests
 
 from keys import Keys
@@ -18,7 +20,9 @@ headers = {
     "x-app-id": app_id,
     "x-app-key": api_key,
 }
-
+sheet_header = {
+    "Authorization": keys.auth
+}
 parameters = {
     "weight_kg": 80,
     "height_cm": 162.56,
@@ -31,3 +35,32 @@ response.raise_for_status()
 result = response.json()
 print(result)
 # name,nf_calories,duration_min,date,time column sheets
+# test_data = [{'tag_id': 317, 'user_input': 'run', 'duration_min': 31.08, 'met': 9.8, 'nf_calories': 406.11,
+#               'photo': {'highres': 'https://d2xdmhkmkbyw75.cloudfront.net/exercise/317_highres.jpg',
+#                         'thumb': 'https://d2xdmhkmkbyw75.cloudfront.net/exercise/317_thumb.jpg',
+#                         'is_user_uploaded': False}, 'compendium_code': 12050, 'name': 'running', 'description': None,
+#               'benefits': None},
+#              {'tag_id': 100, 'user_input': 'walked', 'duration_min': 37.28, 'met': 3.5, 'nf_calories': 173.97,
+#               'photo': {'highres': 'https://d2xdmhkmkbyw75.cloudfront.net/exercise/100_highres.jpg',
+#                         'thumb': 'https://d2xdmhkmkbyw75.cloudfront.net/exercise/100_thumb.jpg',
+#                         'is_user_uploaded': False}, 'compendium_code': 17190, 'name': 'walking', 'description': None,
+#               'benefits': None}]
+
+# extracting data from nutrition api
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(google_sheet_endpoint, json=sheet_inputs,headers=sheet_header)
+
+    print(sheet_response.text)
